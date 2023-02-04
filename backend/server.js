ebc2fb9fc8e87b49a8c69eaa89aa5720691dad45
twdinn <twdinn@gmail.com>
2023-02-03 20:55:01 -0330

    .

--- backend/server.js
+++ backend/server.js
@@ -18,10 +18,6 @@ const port = process.env.PORT || 4000;
 
 app.use(express.json());
 
-// app.get("/api/config/paypal", (req, res) =>
-//   res.send(process.env.PAYPAL_CLIENT_ID)
-// );
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
